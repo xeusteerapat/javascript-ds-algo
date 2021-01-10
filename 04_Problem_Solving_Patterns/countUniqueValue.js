3d8a90fecfb209c805b7e359d7b9e60aaa37266d
xeusteerapat <xeus085@gmail.com>
2021-01-10 23:42:34 +0700

    [ADD] count unique values (colt solution)

--- 04_Problem_Solving_Patterns/countUniqueValue.js
+++ 04_Problem_Solving_Patterns/countUniqueValue.js
@@ -1,5 +1,5 @@
 // My approach
-function countUniqueValues(arr) {
+function countUniqueValues1(arr) {
   const arrObj = arr.reduce((acc, curr) => {
     if (acc[curr]) {
       acc[curr] += 1;
@@ -18,3 +18,18 @@ countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
 countUniqueValues([]); // 0
 countUniqueValues([-2, -1, -1, 0, 1]); // 4
+
+// Colt's solution
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  console.log(i + 1);
+  return i + 1;
+}
